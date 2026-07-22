73b451523762ad2dc5e4c78192da6df7092598d5
Martin weiss <Martin.weiss2410@gmail.com>
2026-07-22 19:35:35 +0700

    added basic first driver

---
+++ basic-driver/ldd.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENCE("GPL");
+MOUDLE_AUTHOR("Martin");
+MODULE_DESCRIPTION("First dynamically loadable kernel module");
+
+static int main_module_init(void) {
+	printk("Hello world! (from kernel module)");
+	return 0;
+}
+
+static void main_module_exit(void) {
+	printk("Good bye! (from kernel module)");
+}
+
+module_init(main_module_init);
+module_exit(main_module_exit);

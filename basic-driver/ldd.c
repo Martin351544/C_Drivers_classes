6c01fc5c2d68d8056e04a3d0ed5aea2f0dbc6442
Martin weiss <Martin.weiss2410@gmail.com>
2026-07-22 19:56:52 +0700

    edited the basic driver -- again

--- basic-driver/ldd.c
+++ basic-driver/ldd.c
@@ -6,12 +6,12 @@ MODULE_AUTHOR("Martin");
 MODULE_DESCRIPTION("First dynamically loadable kernel module");
 
 static int main_module_init(void) {
-	printk("Hello world! (from kernel module)");
+	printk(KERN_INFO"Hello world! (from kernel module)");
 	return 0;
 }
 
 static void main_module_exit(void) {
-	printk("Good bye! (from kernel module)");
+	printk(KERN_INFO "Good bye! (from kernel module)");
 }
 
 module_init(main_module_init);
